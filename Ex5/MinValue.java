c22b59b244b1133ee07a975c506b327b509940e4
Ducnguyn97 <nguyenduc8197@gmail.com>
2025-08-06 16:26:02 +0700

    commit file min value in array 2d

---
+++ Ex5/MinValue.java
@@ -0,0 +1,40 @@
+package Ex5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int colum, row;
+        do {
+            System.out.println("Nhập vào số cột: ");
+            colum = Integer.parseInt(scanner.nextLine());
+            System.out.println("Nhập vào số hàng");
+            row = Integer.parseInt(scanner.nextLine());
+            if (row > 5 || colum > 5) {
+                System.out.println("hàng và cột không lớn hơn 5: ");
+            }
+        } while (row > 5 || colum > 5);
+
+        int[][] matrix = new int[row][colum];
+
+        for (int i = 0; i < row; i++) {
+            System.out.println("Phần tử hàng thứ: " + (i + 1));
+            for (int j = 0; j < colum; j++) {
+                System.out.println("Phần tử thứ " + (j + 1) + " hàng " + (i + 1));
+                matrix[i][j] = Integer.parseInt(scanner.nextLine());
+            }
+        }
+        System.out.println("Mảng: " + Arrays.deepToString(matrix));
+        int min = matrix[0][0];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
+        }
+        System.out.println("GTNN mảng 2 chiều : " + min);
+    }
+}
